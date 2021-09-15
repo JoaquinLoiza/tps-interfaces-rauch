4a4bbf5eb2bab133465295c81668d5e058864cfb
Joaquin Loiza <loiza.joaquin@gmail.com>
2021-09-15 02:42:55 -0300

    centra la imagen en el canvas

--- TP1/EntregaFinal/js/ImageUpload.js
+++ TP1/EntregaFinal/js/ImageUpload.js
@@ -9,13 +9,15 @@ class ImageUpload {
 
     draw(){
         this.resize();
-        this.context.drawImage(this.img, 0,0, this.width, this.height);
+        const x = this.context.canvas.clientWidth / 2 - this.width / 2;
+        const y = this.context.canvas.clientHeight / 2 - this.height / 2;
+        this.context.drawImage(this.img, x,y, this.width, this.height);
     }
 
     resize(){
-        let wCanvas = this.context.canvas.clientWidth;
-        let hCanvas = this.context.canvas.clientHeight;
-        let aspectRatio =(Math.min((wCanvas / this.width), (hCanvas / this.height)));
+        const wCanvas = this.context.canvas.clientWidth;
+        const hCanvas = this.context.canvas.clientHeight;
+        const aspectRatio =(Math.min((wCanvas / this.width), (hCanvas / this.height)));
         this.setWidth(this.width * aspectRatio);
         this.setHeight(this.height * aspectRatio);
     }
