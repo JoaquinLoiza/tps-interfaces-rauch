1a3a032bf2bf6bc48fe24dae682b1ac3d31c8e7b
Joaquin Loiza <loiza.joaquin@gmail.com>
2021-10-07 11:41:15 -0300

    agrega clase tablero y main

---
+++ TP2/EntregaFinal/js/Board.js
@@ -0,0 +1,54 @@
+class Board {
+    constructor(ctx, image, sizeCell){
+        this.ctx = ctx;
+        this.image = image;
+        this.sizeCell = sizeCell;
+        this.columns = 7;
+        this.rows = 6; 
+        this.matrix = null;
+    }
+
+    setColumns(n){
+        this.columns = n;
+    }
+    
+    setRows(n){
+        this.rows = n;
+    }
+
+    setSizeCell(n){
+        this.sizeCell = n;
+    }
+
+    //Dibuja el tablero en base a la matriz formada en el constructor. 
+    draw(){
+        this.matrix = this.createMatrix(this.columns, this.rows); 
+        const cellSize = this.sizeCell; // tamaño del celda
+        const width = this.ctx.canvas.width;
+        const inicioX = width - (width/4)*3; 
+        
+        let y = 0, x = inicioX;
+        for (let row of this.matrix) {
+            x = inicioX;
+            for (let column of row) {
+                this.ctx.drawImage(this.image, x, y, cellSize, cellSize);
+                x += cellSize;
+            }
+            y += cellSize;
+        }
+    }
+
+    //Crea la matriz y la rellena con ceros para saber que esta vacia
+    createMatrix(columns, rows) {
+        let matrix = new Array();
+        let row;
+        for (let i=0; i<rows; i++) {
+	 		row = new Array();
+            for(let j=0; j<columns; j++) {
+                row.push(0);
+            }
+            matrix.push(row);
+        }
+        return matrix;	
+    }
+}
---
+++ TP2/EntregaFinal/js/main.js
@@ -0,0 +1,89 @@
+let myCanvas = document.getElementById('myCanvas');
+let ctx = myCanvas.getContext('2d');
+//Ancho de la seccion donde se encuentran las fichas. 
+//Esta medida nos sirve de referencia para ajustar el canvas en los distintos tipos de juego
+let widthContainerTokens = myCanvas.width /4;
+//calculamos la medida justa de cada celda para ajustar el tablero al centro del canvas
+let sizeCell = (widthContainerTokens*2)/7; 
+//Se ajusta el alto del canvas segun el tablero
+myCanvas.height = sizeCell * 6;
+
+let color = "red";
+let format = "redonda";
+
+//Comienzo
+//Inicialmente, el tablero comienza con "4 en linea" por defecto, dando la opcion al jugador de cambiarlo.
+document.getElementById("btnInitGame").addEventListener("click", initGame);
+
+let imageCell = new Image();
+imageCell.src = "../images/celda.png";
+
+
+//Instanciamos: Juego, Tablero, Ficha, FichasPlayer1, FichasPlayer2. 
+let game = new Game();
+let board;
+let token;
+let tokensPlayer1; //lee del document.getElem.. 
+let tokensPlayer2; 
+
+//Dibuja el tablero (por defecto 4 en linea) al cargar la pagina
+imageCell.onload = function () {
+    board = new Board(ctx, imageCell, sizeCell);
+    board.draw();
+}
+
+function initGame(){
+    let valueSelect = document.getElementById("selectGames").value;
+    if(valueSelect == 4){
+        sizeCell = (widthContainerTokens*2)/7;
+        myCanvas.height = sizeCell*6;
+        board.setColumns(7);
+        board.setRows(6);
+        board.setSizeCell(sizeCell);
+        board.draw();
+        game = new Game(ctx, board, tokensPlayer1, tokensPlayer2);
+        game.tokensDraw();
+    }
+    else if (valueSelect == 5){
+        sizeCell = (widthContainerTokens*2)/8;
+        myCanvas.height = sizeCell*7;
+        board.setColumns(8);
+        board.setRows(7);
+        board.setSizeCell(sizeCell);
+        board.draw();
+    }
+    else if (valueSelect == 6){
+        sizeCell = (widthContainerTokens*2)/9;
+        myCanvas.height = sizeCell*8;
+        board.setColumns(9);
+        board.setRows(8);
+        board.setSizeCell(sizeCell);
+        board.draw();
+    }
+}
+
+function drawRect(){
+    ctx.fillStyle = "#FFB9B9"; //Rojo
+    ctx.fillRect(0, 0, widthContainerTokens, myCanvas.height);
+    ctx.fillStyle = "#FCFFB9"; //Amarillo
+    ctx.fillRect(widthContainerTokens*3, 0, widthContainerTokens, myCanvas.height);
+}
+
+/*let  fichaImg= new Image();
+fichaImg.src = "../images/ficha.png";
+
+function drawGamesTokenIzq(){
+    let fichasIzq = new Array();
+    
+    fichaImg.onload = function () {
+        let ficha = new GameToken(ctx, fichaImg);
+        //si juega con cuatro en linea el array es de 21 fichas 
+        for(let i = 0; i<21; i++){
+        fichasIzq.push(ficha);
+        } 
+    }
+
+    console.log(fichasIzq);
+}
+
+drawGamesTokenIzq();*/
