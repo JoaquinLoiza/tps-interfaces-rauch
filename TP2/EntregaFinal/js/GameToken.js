65e0e40e279eb5dabcce4a04ea35761f225a033c
Romina Dehesa <rominadehesa@gmail.com>
2021-10-07 22:33:36 -0300

    fix agregue funcionalidad en relacion a las fichas

--- TP2/EntregaFinal/js/GameToken.js
+++ TP2/EntregaFinal/js/GameToken.js
@@ -1,16 +1,46 @@
 class GameToken {
-    constructor(ctx, image, sizeToken){
+    constructor(ctx, idPlayer, image, sizeToken){
         this.ctx = ctx;
+        this.idPlayer = idPlayer;
         this.image = image;
         this.sizeToken = sizeToken;
+        this.posX = 0;
+        this.posY = 0;
+    }
 
+    getPosX(){
+        return this.posX;
+    }
+
+    getPosY(){
+        return this.posY; 
+    }
+
+    setPosX(x){
+        this.posX = x;
+    }
+
+    setPosY(y){
+        this.posY = y;
+    }
+
+    getIdPlayer(){
+        return this.idPlayer;
     }
 
     getSizeToken(){
         return this.sizeToken;
     }
 
-    draw(x, y){
-        this.ctx.drawImage(this.image, x, y, this.sizeToken, this.sizeToken);
+    draw(){
+        this.ctx.drawImage(this.image, this.posX, this.posY, this.sizeToken, this.sizeToken);
+    }
+    
+    isPointInside(x, y){
+        let p_x = this.getPosX - x;
+        let p_y = this.getPosY - y;
+        console.log(this.image.width);
+        console.log(this.image.width/2);
+        return Math.sqrt(p_x * p_x + p_y * p_y)<this.image.width/2;
     }
 }
--- TP2/EntregaFinal/js/main.js
+++ TP2/EntregaFinal/js/main.js
@@ -7,42 +7,38 @@ let widthContainerTokens = myCanvas.width /4;
 let sizeCell = (widthContainerTokens*2)/7; 
 //Se ajusta el alto del canvas segun el tablero
 myCanvas.height = sizeCell * 6;
-
-let color = "red";
-let format = "redonda";
-
-//Comienzo
-//Inicialmente, el tablero comienza con "4 en linea" por defecto, dando la opcion al jugador de cambiarlo.
 document.getElementById("btnInitGame").addEventListener("click", initGame);
 
+//Juego
+let game = new Game();
+
+//Tablero
+let board;
+
+// Celdas
 let imageCell = new Image();
 imageCell.src = "images/celda.png";
-
-
-//Instanciamos: Juego, Tablero, Ficha, FichasPlayer1, FichasPlayer2. 
-let game = new Game();
-let board;
-let token;
-let tokensPlayer1; //lee del document.getElem.. 
-let tokensPlayer2; 
-
-
-
-let imageToken = new Image();
-imageToken.src = "images/ficha.png";
-imageToken.onload = function () {
-    tokensPlayer1=createTokens(color, format);
-    tokensPlayer2=createTokens(color, format);
-}
-
-//Dibuja el tablero (por defecto 4 en linea) al cargar la pagina
 imageCell.onload = function () {
     board = new Board(ctx, imageCell, sizeCell);
     board.draw();
 }
 
+//Dibuja el tablero (por defecto 4 en linea) al cargar la pagina
 function initGame(){
+    //Fichas
+    let tokensPlayer1; //array de GameToken
+    let tokensPlayer2; //array de GameToken
+
+    let tokensJson = defineTokensForEachPlayer();
+    //Imagenes de diferentes tipos de fichas
+    let imageToken1 = new Image();
+    imageToken1.src = tokensJson.p1; 
+
+    let imageToken2 = new Image();
+    imageToken2.src = tokensJson.p2;
+
     let valueSelect = document.getElementById("selectGames").value;
+
     if(valueSelect == 4){
         sizeCell = (widthContainerTokens*2)/7;
         myCanvas.height = sizeCell*6;
@@ -50,8 +46,15 @@ function initGame(){
         board.setRows(6);
         board.setSizeCell(sizeCell);
         board.draw();
-        game = new Game(ctx, board, tokensPlayer1, tokensPlayer2);
-        game.tokensDraw();
+        imageToken1.onload = function() {
+            tokensPlayer1 = createTokens(imageToken1, 21, 1);
+        }
+        imageToken2.onload = function() {
+            tokensPlayer2 = createTokens(imageToken2, 21, 2);
+            game = new Game(ctx, board, tokensPlayer1, tokensPlayer2);
+            game.tokensDraw();
+            game.playGame(myCanvas);
+        }
     }
     else if (valueSelect == 5){
         sizeCell = (widthContainerTokens*2)/8;
@@ -71,47 +74,28 @@ function initGame(){
     }
 }
 
-function drawRect(){
-    ctx.fillStyle = "#FFB9B9"; //Rojo
-    ctx.fillRect(0, 0, widthContainerTokens, myCanvas.height);
-    ctx.fillStyle = "#FCFFB9"; //Amarillo
-    ctx.fillRect(widthContainerTokens*3, 0, widthContainerTokens, myCanvas.height);
+function defineTokensForEachPlayer(){
+    let typeToken = document.getElementById("selectToken").value;
+    
+    if(typeToken == 1){
+        return {
+            p1: "images/ficha1.png",
+            p2: "images/ficha2.png"
+        }
+    } else if (typeToken == 2){
+        return {
+            p1: "images/ficha3.png",
+            p2: "images/ficha4.png"
+        }
+    }
+    //other cases ........
 }
 
-function createTokens(color, format){
-    /*
-    if(color == "rojo" && format == "redonda"){
-        token = new GameToken(ctx, imagenTokenRoundRed); 
-    }
-    else if(color == "rojo" && format == "cuadrada"){
-        token = new GameToken(ctx, imagenTokenSquareRed);
-    }
-    */
-
-   let array = new Array();
-   
-   for(let i = 0; i<21; i++){
-        token = new GameToken(ctx, imageToken, sizeCell);
+function createTokens(imageToken, quantity, id){
+    let array = new Array();
+   for(let i = 0; i<quantity; i++){
+        token = new GameToken(ctx, id, imageToken, sizeCell);
         array.push(token);
     }
     return array;
 }
-
-/*let  fichaImg= new Image();
-fichaImg.src = "../images/ficha.png";
-
-function drawGamesTokenIzq(){
-    let fichasIzq = new Array();
-    
-    fichaImg.onload = function () {
-        let ficha = new GameToken(ctx, fichaImg);
-        //si juega con cuatro en linea el array es de 21 fichas 
-        for(let i = 0; i<21; i++){
-        fichasIzq.push(ficha);
-        } 
-    }
-
-    console.log(fichasIzq);
-}
-
-drawGamesTokenIzq();*/
