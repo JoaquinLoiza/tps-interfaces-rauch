405eebe4b7b9ec5960327fc659f5071123cfb74f
Joaquin Loiza <loiza.joaquin@gmail.com>
2021-12-07 11:15:05 -0300

    agrega like y dislike

--- TP5/EntregaFinal/js/main-home.js
+++ TP5/EntregaFinal/js/main-home.js
@@ -1,17 +1,15 @@
 'use strict';
 
-let elDiv = document.getElementById("div_principal");
-elDiv.addEventListener("click", setStyleDiv, false);
-
-function setStyleDiv(){
-    document.querySelector(".seleccionar-chat").style.backgroundColor = "rgb(235 235 235)";
-    document.querySelector(".active-chat1").style.color = "black";
-    document.querySelector(".active-chat2").style.color = "black";
-    document.querySelector(".input-c").style.backgroundColor = "white";
-}
-
 let iconomsj = document.getElementById("icono-msj");
 iconomsj.addEventListener("click", () => {
     document.querySelector(".bi-house-door").style.border = "none";
 });
 
+let likeDislike = document.querySelectorAll(".like-dislike");
+
+for (let i=0; i < likeDislike.length; i++){
+    likeDislike[i].addEventListener("click", function(){
+        likeDislike[i].childNodes[1].classList.toggle("icono_ocultar");
+        likeDislike[i].childNodes[3].classList.toggle("icono_ocultar");
+    });
+}
