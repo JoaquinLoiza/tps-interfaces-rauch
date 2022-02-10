c4b508f9d4534a87ed0153f395c9b12ae7180b0e
Joaquin Loiza <loiza.joaquin@gmail.com>
2022-02-10 19:09:56 -0300

    agrega estilos y estructura al contenedor del header

--- reentrega/src/Components/Header.svelte
+++ reentrega/src/Components/Header.svelte
@@ -1,11 +1,27 @@
 <script>
-
 </script>
 
 <main>
-    <h1>VETUNITY</h1>
+    <header>
+        <div>
+            <img src="./static/Logo_nav.svg" alt="">
+        </div>
+    </header>
 </main>
 
 <style>
+    header {
+        background-color: #544179;
+        height: 60px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 
+    div {
+        background-color: rgb(97, 150, 104);
+        max-width: 1200px;
+        width: 100%;
+        height: 30px;
+    }
 </style>
