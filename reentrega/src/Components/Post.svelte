7f33b06291926f6cc7656f49e7331430bb25c949
Romina Dehesa <rominadehesa@gmail.com>
2022-02-14 09:51:32 -0300

    pruebas

--- reentrega/src/Components/Post.svelte
+++ reentrega/src/Components/Post.svelte
@@ -10,6 +10,11 @@
     Share,
     HeartFill
   } from "svelte-bootstrap-icons";
+
+	export let name;
+  export let text;
+  export let urlImg;
+
 </script>
 
 <main>
@@ -18,7 +23,7 @@
           <div class="card-h1">
             <div class="photo-post"></div>
             <div>
-              <h1>Mariano Alvarez</h1>
+              <h1>{name}</h1>
             </div>
           </div>
           <div class="card-h2">
@@ -28,10 +33,7 @@
         </div>
         <div class="card-body">
           <p>
-            Los médicos veterinarios no sólo se forman para atender a pequeños y
-            grandes animales sino que además lo hacen en un área que cada vez adquiere
-            mayor relevancia y visibilidad en la población a nivel nacional e
-            internacional.
+            {text}
           </p>
         </div>
         <div class="card-head">
@@ -76,6 +78,7 @@
     border: 1px solid;
     border-color: #414759;
     border-radius: 50%;
+    background-image: url('https://papelmatic.com/wp-content/uploads/2019/09/papelmatic-higiene-profesional-limpieza-desinfeccion-clinicas-veterinarias-1200x900.jpg');
   }
   h1 {
     font-size: 16px;
