0c783d763e4afb3e762e6f3277885ef15a51e7fc
Joaquin Loiza <loiza.joaquin@gmail.com>
2022-02-16 00:45:44 -0300

    agrega la tarjeta del perfil con su imagen

--- reentrega/src/Components/Homepage.svelte
+++ reentrega/src/Components/Homepage.svelte
@@ -1,32 +1,41 @@
 <script>
 	import Post from './Post.svelte';
+	import ProfileCard from './ProfileCard.svelte';
 </script>
 
 <main>
     <div class="homepage">
-		<div class="homepage-left">lateral izq</div>
+		<div class="homepage-left">
+			<ProfileCard/>
+		</div>
 		<div class="homepage-center">
 			<Post name={'Juan Lopez'} text={'Los médicos veterinarios no sólo se forman para atender a pequeños y grandes animales sino que además lo hacen en un área que cada vez adquiere mayor relevancia y visibilidad en la población a nivel nacional e internacional.'}/>
 			<Post name={'Fernando Swchard'} text={'Las familias con mascota deben contar con un veterinario de confianza desde el primer momento que el animal llega a nuestros hogares y forma parte de nuestra familia como un miembro más, que siempre estará dispuesto a resolver las dudas que puedan surgir en el día a día.'}/>
-			
 		</div>
-	    <div class="homepage-right">persons que quizas conoscas</div>
+	    <div class="homepage-right">
+			persons que quizas conoscas
+		</div>
 	</div>
 </main>
 
 <style>
-.homepage {
-    display: flex;
-    justify-content: space-between;
-}
-.homepage-left {
-	width: 25%;
-}
-.homepage-center {
-	width: 50%;
-	padding: 20px;
-}
-.homepage-right {
-	width: 25%;
-}
+	main {
+		display: flex;
+		justify-content: center;
+	}
+	.homepage {
+		display: flex;
+	    justify-content: space-between;
+		max-width: 1200px;
+	}
+	.homepage-left {
+		width: 25%;
+	}
+	.homepage-center {
+		width: 50%;
+		padding: 20px;
+	}
+	.homepage-right {
+		width: 25%;
+	}
 </style>
---
+++ reentrega/src/Components/ProfileImage.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let pathImage;
+</script>
+
+<div style="background-image: url(../{pathImage})"></div>
+
+<style>
+    div{
+        width: 120px;
+        height: 120px;
+        background-size: 100%;
+        border-radius: 50%;
+        border: 1px solid;
+        border-color: #414759;
+    }
+</style>
+
+
