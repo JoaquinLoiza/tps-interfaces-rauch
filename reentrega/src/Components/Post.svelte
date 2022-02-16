e961727f4c5aa36991586c42da3cf6377c4b36d6
Romina Dehesa <rominadehesa@gmail.com>
2022-02-16 00:42:24 -0300

    fix componente post

--- reentrega/src/Components/Post.svelte
+++ reentrega/src/Components/Post.svelte
@@ -3,22 +3,46 @@
     ChatSquareText,
     HandThumbsUp,
     HandThumbsDown,
+    HandThumbsUpFill,
+    HandThumbsDownFill,
     Share,
     ThreeDotsVertical
   } from "svelte-bootstrap-icons";
 
 	export let name;
   export let text;
-  export let urlImg;
+  export let countLikes = 12;
 
-  let like = false;
-  let dislike = true;
-  let c
+  let state = { liked: false, dislike:false, comments:false};
+  
   function giveLike(){
-    
+    if(state.liked == false) {
+      state.liked = true;
+      countLikes++;
+    }
+    else {
+      countLikes--;
+      state.liked = false;
+    }
   }
 
+  function giveDisLike(){
+    if(state.dislike == false){
+      state.dislike = true;
+    }
+    else {
+      state.dislike = false;
+    }
+  }
 
+  function showComments(){
+    if(state.comments == false){
+      state.comments = true;
+    }
+    else {
+      state.comments = false;
+    }
+  }
 
 </script>
 
@@ -27,31 +51,47 @@
         <div class="card-head">
           <div class="card-h1">
             <div class="photo-post"></div>
-            <div>
-              <h1>{name}</h1>
+            <div class="name-time">
+              <div><h1>{name}</h1></div>
+              <div class="time">14 de Febrero del 2022 a las 15:30hs.</div>
             </div>
           </div>
           <div class="card-h2">
-            <div class="time">3 min</div>
             <div><ThreeDotsVertical/></div>
           </div>
         </div>
+        <!--Cuerpo del post-->
         <div class="card-body">
           <p>
             {text}
           </p>
         </div>
-        <div class="card-head">
-          <div class="card-h1">
-            <div on:click={giveLike}><HandThumbsUp/></div>
-            <div><HandThumbsDown/></div>
+        <!--Footer del post-->
+        <div class="card-f">
+          <div class="card-f1">
+            <div>
+              <span>{countLikes}</span>
+            </div>
+            {#if !state.liked}
+            <div on:click={giveLike}>
+              <HandThumbsUp/>
+            </div>
+            {:else}
+            <div on:click={giveLike}><HandThumbsUpFill/></div>
+            {/if}
+
+            {#if !state.dislike}
+            <div on:click={giveDisLike}><HandThumbsDown/></div>
+            {:else}
+            <div on:click={giveDisLike}><HandThumbsDownFill/></div>
+            {/if}
+
             <div><ChatSquareText/></div>
             <div><Share/></div>
           </div>
       
-          <div class="card-h2">
-            <div>10 Me gusta</div>
-            <div>2 Comentarios</div>
+          <div class="card-f2">
+            <p on:click={showComments}>3 Comentarios</p>
           </div>
         </div>
       </div>
@@ -64,29 +104,29 @@
     border-radius: 10px;
     background-color: white;
     box-shadow: 5px 5px 16px -1px rgba(0, 0, 0, 0.2);
-    padding: 10px;
+    margin-bottom: 10px;
     margin-top: 10px;
   }
-  .card-head {
+  .name-time {
+    margin-left: 5px;
+  }
+  .card-head, .card-f {
     display: flex;
     justify-content: space-between;
+    align-items: center;
   }
-  .card-h1 {
+  .card-h1, .card-f1 {
     display: flex;
     justify-content: start;
+    align-items: center;
   }
-  .card-h1 div {
-    margin: 5px;
-  }
-  .card-h2 {
+  .card-h2, .card-f2 {
     display: flex;
     justify-content: end;
-    padding-right: 5px;
   }
   .card-h2 div{
     font-size: 12px;
     color: #AAAAAA;
-    margin-top: 2px;
   }
   .photo-post {
     height: 35px;
@@ -98,17 +138,34 @@
     background-image: url('https://papelmatic.com/wp-content/uploads/2019/09/papelmatic-higiene-profesional-limpieza-desinfeccion-clinicas-veterinarias-1200x900.jpg');
   }
   h1 {
-    font-size: 16px;
+    font-size: 17px;
     font-weight: 600;
     color: #414759;
-    padding-left: 5px;
+    margin-bottom: 0;
   }
   p {
-    font-size: 12px;
+    font-size: 15px;
     font-weight: 600;
   }
-  .icons-post {
-    width: 50px;
-    height: 50px;
+  div :global(svg){
+    height: 18px;
+    width: 18px;
+    padding-right: 10px;
+  }
+  .time {
+    color:#414759;
+    font-size: 12px;
+
+  }
+  span {
+    height: 20px;
+    width: 20px;
+  }
+  .card-head, .card-body, .card-f {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+  .card-f2 p {
+    font-size: 12px;
   }
 </style>
