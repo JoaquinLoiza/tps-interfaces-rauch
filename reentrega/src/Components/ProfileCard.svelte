74d819461111683b2693c4ff6c693b6189dc5014
Joaquin Loiza <loiza.joaquin@gmail.com>
2022-02-16 12:12:13 -0300

    agrega estilos y posicionamiento

--- reentrega/src/Components/ProfileCard.svelte
+++ reentrega/src/Components/ProfileCard.svelte
@@ -2,17 +2,38 @@
     import ProfileImage from './ProfileImage.svelte';
 </script>
 
-<div>
+<div class="container">
     <div class="image">
-        <ProfileImage pathImage={'images/img_profile.jpeg'}/>
+        <ProfileImage height={120} width={120} pathImage={'../images/img_profile.jpeg'}/>
     </div>
     <div class="name">
-
+        <h1>Jose Perez</h1>
+        <button>Mi perfil</button>
     </div>
 </div>
 
 <style>
     .image, .name{
         width: 50%;
+    }
+
+    .container {
+        height: 150px;
+        display: flex;
+        align-items: center;
+        border-radius: 10px;
+        background-color: white;
+        box-shadow: 5px 5px 16px -1px rgba(0, 0, 0, 0.2);
+        margin-top: 30px;
+    }
+
+    .image {
+        display: flex;
+        justify-content: center;
+    }
+
+    h1 {
+        font-size: 20px;
+        margin-top: 0;
     }
 </style>
--- reentrega/src/Components/ProfileImage.svelte
+++ reentrega/src/Components/ProfileImage.svelte
@@ -1,18 +1,16 @@
 <script>
     export let pathImage;
+    export let height;
+    export let width;
 </script>
 
-<div style="background-image: url(../{pathImage})"></div>
+<div style="height:{height}px; width:{width}px; background-image: url({pathImage})"></div>
 
 <style>
     div{
-        width: 120px;
-        height: 120px;
         background-size: 100%;
         border-radius: 50%;
         border: 1px solid;
         border-color: #414759;
     }
 </style>
-
-
