cfd97633e57562a506fb574b19a73c7c1de8078f
Joaquin Loiza <loiza.joaquin@gmail.com>
2022-02-19 01:13:54 -0300

    estilos de boton postear

--- reentrega/src/Components/NewPost.svelte
+++ reentrega/src/Components/NewPost.svelte
@@ -1,10 +1,12 @@
 <script>
+    import XSquare from "svelte-bootstrap-icons/lib/XLg";
     import ButtonApp from "./ButtonApp.svelte";
 </script>
 
 <div class="post">
     <div class="post-a">
         <p>Compartir publicación</p>
+        <XSquare/>
     </div>
     <div class="post-b">
         <textarea placeholder="¿Qué estás pensando?"></textarea>
@@ -12,33 +14,51 @@
     <div class="post-c">
         <ButtonApp text={'Postear'} width={120} size={15}/>
     </div>
-    
 </div>
 
 <style>
+    p {
+        color: #414759;
+    }
+
+    .post-a :global(svg) {
+        margin-right: 2px;
+        color: #414759;
+    }
+
     .post {
-		width: 300px;
+		width: 590px;
 		border-radius: 10px;
   		box-shadow: 5px 5px 16px -1px rgba(0,0,0,0.2);
   		background-color: rgb(235, 235, 235);
 	}
 	.post-a {
+        width: 546px;
 		display:flex;
 		justify-content: space-between;
 		align-items:center;
         text-align: center;
+        margin: 8px 24px 0 24px;
 	}
+
+    .post-b {
+        margin: 0 24px 0 24px;
+    }
+
     .post-c{
         display:flex;
-        justify-content:center;
+        justify-content:space-around;
         text-align: center;
+        margin: 2px 0 6px 0;
     }
     textarea {
+        min-height: 200px;
+        font-size: 15px;
+        font-family: Nunito,'Courier New', Courier, monospace;
+        width: 100%;
         resize: none;
         outline: none;
         border: none;
         border-radius: 10px;
-        width: 80%;
-        margin: 5px;
     }
 </style>
