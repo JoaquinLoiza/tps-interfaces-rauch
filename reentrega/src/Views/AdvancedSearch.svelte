64542e873f9cfa90d7aededb016e070254beaf0f
Joaquin Loiza <loiza.joaquin@gmail.com>
2022-02-23 01:03:12 -0300

    agrega buttons outlines

--- reentrega/src/Views/AdvancedSearch.svelte
+++ reentrega/src/Views/AdvancedSearch.svelte
@@ -1,6 +1,40 @@
 <script>
     import Header from '../Components/Header.svelte';
+    import ButtonOutline from '../Components/ButtonOutline.svelte';
 </script>
 
 <Header />
-<h1>search</h1>
+<main>
+    <div class="btnsOutlines">
+        <div>
+            <ButtonOutline text={'Todo'} width={90} size={15}/>
+        </div>
+        <div>
+            <ButtonOutline text={'Personas'} width={90} size={15}/>
+        </div>
+        <div>
+            <ButtonOutline text={'Publicaciones'} width={100} size={15}/>
+        </div>
+        <div>
+            <ButtonOutline text={'Lugares'} width={90} size={15}/>
+        </div>
+        <div>
+            <ButtonOutline text={'Ocupación'} width={90} size={15}/>
+        </div>
+    </div>
+</main>
+
+<style>
+    main {
+        display: flex;
+        justify-content: center;
+    }
+    
+    .btnsOutlines {
+        margin-top: 20px;
+        width: 100%;
+        max-width: 1200px;
+        display: flex;
+        justify-content: center;
+    }
+</style>
